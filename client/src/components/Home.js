a3408710588cea33669ac2e37b2f681d21e4dc73
himanshu savita <himanshusavita@himanshus-Air.gateway-fw.fw>
2018-12-03 10:50:30 +0530

    home page features

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,14 +2,34 @@ import React, {Component, Fragment} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import * as auth from '../auth/auth';
 
 
 const styles = theme => ({
     landingPage:{
-      backgroundImage: "url('http://portobsb.adminbsb-themes.com/assets/images/main_bg.jpg')",
-      backgroundSize: "cover",
-      height: "636px"
+       position: 'relative'
+    },
+    coverImage:{
+        width:'100%'
+    },
+    homeActions:{
+      top: '15%',
+      position: 'absolute',
+      left: '10%',
+      width: '50%'
+    },
+    homeHeading:{
+        color:'#fff',
+        marginBottom: '20px'
+    },
+    buildResumeButton:{
+      marginRight: '30px'
+    },
+    colorHeading: {
+       color: "#000"
     }
 });
 
@@ -24,7 +44,23 @@ class Home extends Component{
           const {classes} = this.props;
           return(
              <div className={classes.landingPage}>
-
+                <img className={classes.coverImage} src="http://portobsb.adminbsb-themes.com/assets/images/main_bg.jpg"/>
+                <div className={classes.homeActions}>
+                    <div className={classes.homeHeading}>
+                      <Typography  className={classes.colorHeading} component="h2" variant="display1">
+                        Your Resume, Made Easy
+                      </Typography>
+                      <Typography component="h4" variant="headline" gutterBottom>
+                        My Perfect Resume takes the hassle out of resume
+                        writing. Easy prompts help you create the perfect
+                        job-worthy resume effortlessly!
+                      </Typography>
+                    </div>
+                    <div>
+                      <Button color="primary" variant="contained" className={classes.buildResumeButton} onClick={this.facebookLogin}>Build Resume</Button>
+                      <Button color="primary" variant="contained" className={classes.facebooklogin} onClick={this.facebookLogin}>Edit and Download Sample Template</Button>
+                    </div>
+                </div>
              </div>
           )
       }
